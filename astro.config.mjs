855af37eaaec7eaad1b537a1e40119797c9d4870
Andres felipe Sierra <asierra447@gmail.com>
2024-06-03 13:52:01 -0500

    cambios en datos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://andressb22.github.io',
-    base: '/28A-osDeInvovacion',
+    base: '/inovacion',
 });
